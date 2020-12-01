b107d44a7ee854c137a32eaea4a1367ca24dec7e
Allen Reinmeyer <allen.reinmeyer@sap.com>
2020-12-01 15:56:26 -0500

    Simple working Day 1

---
+++ Day1/index.mjs
@@ -0,0 +1,55 @@
+import { readFileSync } from 'fs'
+/*
+    Read the puzzleInput as a string then split it into an array
+    Create a new array converting the strings to ints.
+    A more robust version would make sure the parseInt function doesn't blow up
+*/
+const readModuleList = (filename) => {
+    const rawInput = readFileSync(filename, 'utf-8')
+    const inputAsArray = rawInput.split('\n')
+    const iModules = inputAsArray.map(function (item) {
+        return parseInt(item, 10)
+    })
+    // console.log(iModules)
+    return iModules
+}
+
+
+const find2020with2numbers = (numberArray) => {
+    numberArray.forEach(element => {
+        const clonedArray = new Set([...numberArray])
+        clonedArray.delete(element);
+        [...clonedArray].map(num => {
+            if (num + element === 2020 ) {
+                console.log(`${num} + ${element} is 2020`);
+                console.log(num * element)
+                return num * element;
+            }
+        })
+
+    });
+}
+
+const find2020with3numbers = (numberArray) => {
+    numberArray.forEach(element => {
+        const clonedArray = new Set([...numberArray])
+        clonedArray.delete(element);
+        [...clonedArray].map(num => {
+            if (num + element < 2020 ) {
+                const smallerArray = new Set([...clonedArray])
+                smallerArray.delete(num);
+                [...smallerArray].map(thirdNum => {
+                    if (num + element + thirdNum === 2020) {
+                        console.log(`${num} + ${element} + ${thirdNum}`)
+                        console.log(num * element * thirdNum)
+                    }
+                })
+                return num * element;
+            }
+        })
+
+    });
+}
+
+find2020with2numbers(readModuleList(`input.txt`));
+find2020with3numbers(readModuleList(`input.txt`));
