53ddcb48a03fdab55a255a189ec84332517dc560
Allen Reinmeyer <areinmeyer@gmail.com>
2020-12-04 15:22:47 -0500

    updates to logic

--- src/Day1/index.ts
+++ src/Day1/index.ts
@@ -10,42 +10,51 @@ const readModuleList = (filename: string) => {
   const iModules = inputAsArray.map(function (item) {
     return parseInt(item, 10);
   });
-  // console.log(iModules)
   return iModules;
 };
 
+/**
+ * Part 1
+ * @param numberArray list of expenses
+ */
 const find2020with2numbers = (numberArray: Array<number>) => {
-  numberArray.forEach((element) => {
-    const clonedArray = new Set([...numberArray]);
-    clonedArray.delete(element);
-    [...clonedArray].map((num) => {
-      if (num + element === 2020) {
-        console.log(`${num} + ${element} is 2020`);
-        console.log(num * element);
-        return num * element;
-      }
-    });
-  });
+  console.log("Finding 2 numbers that sum 2020");
+  for (let i = 0; i < numberArray.length; i++) {
+    const startNumber = numberArray[i];
+    const numberSet = new Set(numberArray);
+    numberSet.delete(startNumber);
+    const diff = 2020 - startNumber;
+    if (numberSet.has(diff)) {
+      console.log(`${startNumber} + ${diff} is 2020`);
+      console.log(startNumber * diff);
+      return startNumber * diff;
+    }
+  }
 };
 
+/**
+ * Part 2
+ * @param numberArray list of expenses
+ */
 const find2020with3numbers = (numberArray: Array<number>) => {
-  numberArray.forEach((element) => {
-    const clonedArray = new Set([...numberArray]);
-    clonedArray.delete(element);
-    [...clonedArray].map((num) => {
-      if (num + element < 2020) {
-        const smallerArray = new Set([...clonedArray]);
-        smallerArray.delete(num);
-        [...smallerArray].map((thirdNum) => {
-          if (num + element + thirdNum === 2020) {
-            console.log(`${num} + ${element} + ${thirdNum}`);
-            console.log(num * element * thirdNum);
-          }
-        });
-        return num * element;
+  console.log("Finding 3 numbers that sum 2020");
+  for (let i = 0; i < numberArray.length; i++) {
+    const startNumber = numberArray[i];
+    for (let j = i + 1; j < numberArray.length; j++) {
+      const secondNumber = numberArray[j];
+      if (secondNumber + startNumber < 2020) {
+        const numberSet = new Set(numberArray);
+        numberSet.delete(secondNumber);
+        numberSet.delete(startNumber);
+        const diff = 2020 - secondNumber - startNumber;
+        if (numberSet.has(diff)) {
+          console.log(`${secondNumber} + ${startNumber} + ${diff}`);
+          console.log(secondNumber * startNumber * diff);
+          return secondNumber * startNumber * diff;
+        }
       }
-    });
-  });
+    }
+  }
 };
 
 find2020with2numbers(readModuleList(`${__dirname}/input.txt`));
